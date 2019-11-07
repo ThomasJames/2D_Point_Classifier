f05162075f88b1e283c53cde58d9030b1539c1ab
ThomasJames <tomjamescontact@gmail.com>
2019-11-07 15:18:56 +0000

    Succesful testing on RCA, MBR, and POL - Plot test No I will attempt to make code object oriented

--- main from file.py
+++ main from file.py
@@ -45,87 +45,83 @@ def minimum_bound(p_x, p_y, a_x, a_y):
     else:
         out.append("outside")  # else the point cant be inside or on boundry, therefore its outside
 
+
+
+
+
+def minimum_bound(p_x, p_y, a_x, a_y):
+    if min(a_x) < p_x < max(a_x) and min(a_y) < p_y < max(a_y):  # is the point within the bounds of the box
+        return True
+    elif min(a_x) == p_x and min(a_y) <= p_y <= max(a_y):
+        return True
+    elif max(a_x) == p_x and min(a_y) <= p_y <= max(a_y):
+        return True
+    elif min(a_y) == p_y and min(a_x) <= p_x <= max(a_x):
+        return True
+    elif max(a_y) == p_y and min(a_x) <= p_x <= max(a_x):
+        return True
+    else:
+        return False  # else the point cant be inside or on boundry, therefore its outside
+
+
+
+
+# def merge_lists(point_x, point_y, shape_x, shape_y):
+#     s = []
+#     p = []
+#     for i in range(len(point_x)):
+#         p.append(point_x, point_y)
+#     for i in range(len(shape_x)):
+#         s.append(shape_x, shape_y)
+
+
 def ray_casting(p, s):
     n = len(s)-1
     counter = 0
     for i in range(n):
-        if (s[i][1] <= p[1] and s[i+1][1] > point[1]) or (s[i][1] > point[1] and s[i+1][1] <= p[1]):
+        if (s[i][1] <= p[1] and s[i+1][1] > p[1]) or (s[i][1] > p[1] and s[i+1][1] <= p[1]):
             vt = (p[1] - s[i][1]) / (s[i+1][1] - s[i][1])
-        if p[0] < (s[i][0] + vt * (s[i+1][0] - s[i][0])):
-            counter += 1
+            if p[0] < (s[i][0] + vt * (s[i+1][0] - s[i][0])):
+                counter += 1
         i += 1
     return counter
 
 
-def compute_y(x, x1, y1, x2, y2):
+def calculate_y(x, x1, y1, x2, y2):
     return (x - x1) / (x2 - x1) * (y2 - y1) + y1
 
-def is_on_the_line(p_x, p_y, a_x, a_y, b_x, b_y):
+
+def is_on_line(p_x, p_y, a_x, a_y, b_x, b_y):
     if not ((a_y <= p_y <= b_y or b_y <= p_y <= a_y) and (a_x <= p_x <= b_x or b_x <= p_x <= a_x)):
         return False
     elif a_x == b_x and p_x == a_x:
         return True
-    elif a_x != b_x and compute_y(p_x, a_x, a_y, b_x, b_y) == p_y:
+    elif a_x != b_x and calculate_y(p_x, a_x, a_y, b_x, b_y) == p_y:
         return True
     else:
         return False
 
-box = [(1,1),(1,5),(5,5),(5,1)]
-point = [0,1]
 
-if ray_casting(point, box) %2:
-    print("inside")
-else:
-    print("outside")
-
-
-
+def read_points(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()[1:]
+        for line in lines:
+            line = line.strip().split(',')
+            point_x.append(float(line[1]))
+            point_y.append(float(line[2]))
 
 
+def read_polygon(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()[1:]
+        for line in lines:
+            line = line.strip().split(',')
+            shape_x.append(float(line[1]))
+            shape_y.append(float(line[2]))
 
 
-class File_reader:
-
-    def read_points(filename):
-        with open(filename, 'r') as f:
-            lines = f.readlines()[1:]
-            for line in lines:
-                line = line.strip().split(',')
-                point_x.append(float(line[1]))
-                point_y.append(float(line[2]))
-
-
-    def read_polygon(filename):
-        with open(filename, 'r') as f:
-            lines = f.readlines()[1:]
-            for line in lines:
-                line = line.strip().split(',')
-                shape_x.append(float(line[1]))  # values added to the empty point list.
-                shape_y.append(float(line[2]))
-
-#maybe try to put the merge function outside of create coordinates - then call it from within.
-
-
-    def create_polygon_coordinates(filename):
-         with open(filename, 'r') as f:
-             lines = f.readlines()[1:]
-             for line in lines:
-                 line = line.strip().split(',')
-                 shape_x.append(float(line[1]))
-                 shape_y.append(float(line[2]))
-
-
-    def create_point_coordinates(filename):
-          with open(filename, 'r') as f:
-              lines = f.readlines()[1:]
-              for line in lines:
-                  line = line.strip().split(',')
-                  point_x.append(float(line[1]))  # values added to the empty shape list.
-                  point_y.append(float(line[2]))
-                  point_coordinates = [(point_x[i], point_y[i]) for i in range(0, len(point_x))]
-                  return point_coordinates
-
-
+def generate_coordinates(p_x, p_y):
+    return list(map(lambda x, y:(x,y), p_x, p_y))
 
 
 if __name__ == "__main__":
@@ -134,23 +130,85 @@ if __name__ == "__main__":
     point_y = []
     shape_x = []
     shape_y = []
-    point_on_line = []
-    polygon_coordinates = []
-    point_coordinates = []
-    out = []
-
-    File_reader.read_polygon("polygon.csv")
-    File_reader.read_points("input.csv")
-    File_reader.create_polygon_coordinates("polygon.csv")
-    File_reader.create_point_coordinates("input.csv")
+    s = []
+    p = []
 
 
+    read_polygon("polygon.csv")
+    read_points("input.csv")
+
+    p = generate_coordinates(point_x, point_y)
+    s = generate_coordinates(shape_x, shape_y)
+
+    location = [None] * len(point_x)    # Create an list with 100 empty values
+
+
+    for i in range(len(location)):
+        if (ray_casting(p[i], s)) % 2 != 0:
+            location[i] = ("inside")
+        else:
+            location[i] = ("outside")
+        for j in range(len(shape_x)-1):
+            if is_on_line(point_x[i], point_y[i], shape_x[j], shape_y[j], shape_x[j+1], shape_y[j+1]) == True:
+                location[i]=("boundary")
 
 
 
-    print(polygon_coordinates)
 
-    # plotter = Plotter()
-    # plotter.add_point()
-    # plotter.add_polygon(shape_x, shape_y)
-    # plotter.show()
+    plotter = Plotter()
+    for i in range(len(point_x)):
+        plotter.add_point(point_x[i], point_y[i], location[i])
+    plotter.add_polygon(shape_x, shape_y)
+    plotter.show()
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
